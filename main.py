436c8068c1a3311bc82b8b34d16c83846d20bca2
Giulio <giuliohome@gmail.com>
2022-02-06 15:08:37 +0000

    switch to Fask on GKC

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
+from flask import Flask
 import pyodbc
-print("About to conn")
-#Add your own SQL Server IP address, PORT, UID, PWD and Database
-conn = pyodbc.connect(
-        'DRIVER={FreeTDS};SERVER=34.134.105.142;PORT=1433;DATABASE=firstdb;UID=sqlserver;PWD=Giulio2022', autocommit=False)
 
-conn.setencoding('utf-8')
+app = Flask(__name__)
 
-cursor = conn.cursor()
+@app.route("/")
+def home():
+    return "hello world"
 
-cursor.execute('''
-		CREATE TABLE products (
-			product_id int primary key,
-			product_name nvarchar(50),
-			price int
-			)
-               ''')
 
-conn.commit()
+def sqlconnect():
+    print("About to conn")
+    #Add your own SQL Server IP address, PORT, UID, PWD and Database
+    conn = pyodbc.connect(
+            'DRIVER={FreeTDS};SERVER=10.29.112.3;PORT=1433;DATABASE=firstdb;UID=sqlserver;PWD=Giulio2022', autocommit=False)
 
-cursor.execute('''
-		INSERT INTO products (product_id, product_name, price)
-		VALUES
-			(1,'Desktop Computer',800),
-			(2,'Laptop',1200),
-			(3,'Tablet',200),
-			(4,'Monitor',350),
-			(5,'Printer',150)
+    conn.setencoding('utf-8')
+
+    cursor = conn.cursor()
+
+    cursor.execute('''
+            CREATE TABLE products (
+                product_id int primary key,
+                product_name nvarchar(50),
+                price int
+                )
                 ''')
-conn.commit()
 
-#This is just an example
+    conn.commit()
 
-print('Should have inserted the rows above')
-cursor.close()
-conn.close()
+    cursor.execute('''
+            INSERT INTO products (product_id, product_name, price)
+            VALUES
+                (1,'Desktop Computer',800),
+                (2,'Laptop',1200),
+                (3,'Tablet',200),
+                (4,'Monitor',350),
+                (5,'Printer',150)
+                    ''')
+    conn.commit()
+
+    #This is just an example
+
+    print('Should have inserted the rows above')
+    cursor.close()
+    conn.close()
+
+if __name == '__main__':
+    app.run(debug=True, host='0.0.0.0')
