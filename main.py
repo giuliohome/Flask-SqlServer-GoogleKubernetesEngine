b23628475b0a06cebccfd8d914d17c847693faf5
Giulio <giuliohome@gmail.com>
2022-02-14 12:36:48 +0000

    gunicorn inside container

--- main.py
+++ main.py
@@ -67,4 +67,4 @@ def test_sql():
     return "<html><body>" + s + "</body></html>"  
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080))
