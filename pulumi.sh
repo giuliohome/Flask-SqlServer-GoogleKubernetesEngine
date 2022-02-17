6ba920231479c6e04da3ee9c3eeaf7ee68f6031e
giuliohome <giuliohome@users.noreply.github.com>
2022-02-17 08:00:52 +0100

    Update pulumi.sh

--- pulumi.sh
+++ pulumi.sh
@@ -28,4 +28,10 @@ pulumi config set dockerPassword --secret $DOCKER_PSWD
 pulumi config set masterVersion latest
 set -o history
 
+# gcloud needed for deployment by pulumi
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+apt-get install apt-transport-https ca-certificates gnupg curl -y
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+apt-get update && apt-get install google-cloud-sdk -y
+
 pulumi up --yes
