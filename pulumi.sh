a94db8d963361681f21525e0de113a53450fac19
giuliohome <giuliohome@users.noreply.github.com>
2022-02-17 01:45:45 +0100

    curl docker

--- pulumi.sh
+++ pulumi.sh
@@ -28,7 +28,7 @@ pulumi config set dockerPassword --secret $DOCKER_PSWD
 pulumi config set masterVersion latest
 set -o history
 
-sudo curl -sSL https://get.docker.com/ | sh
+curl -sSL https://get.docker.com/ | sh
 docker build . -t giuliohome/gcp-flask-sqlserver-k8s
 
 pulumi up --yes
